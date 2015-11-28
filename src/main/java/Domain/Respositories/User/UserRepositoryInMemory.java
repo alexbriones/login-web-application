c9bd54cf153cc4bf18b27501988e1f98ffff8e0d
alex.briones <alex.briones@atrapalo.com>
2015-11-28 23:57:27 +0100

    login web application

--- src/main/java/Domain/Respositories/User/UserRepositoryInMemory.java
+++ src/main/java/Domain/Respositories/User/UserRepositoryInMemory.java
@@ -6,21 +6,50 @@ import Domain.Model.User.UserRepository;
 import Domain.Model.User.Password;
 import Domain.Model.User.Role;
 import Domain.Model.User.User;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class UserRepositoryInMemory implements UserRepository{
+public class UserRepositoryInMemory implements UserRepository {
 
     private final HashMap<String, User> users = new HashMap<>();
 
     public UserRepositoryInMemory() {
         try {
-            saveUser(new User("user_one", new Password("one"), new HashSet<>((Arrays.asList((Role.PAGE_1))))));
-            saveUser(new User("user_two", new Password("two"), new HashSet<>((Arrays.asList((Role.PAGE_2))))));
-            saveUser(new User("user_three", new Password("three"), new HashSet<>((Arrays.asList((Role.PAGE_3))))));
-            saveUser(new User("user_four", new Password("four"), new HashSet<>((Arrays.asList((Role.PAGE_1), Role.PAGE_2)))));
-            saveUser(new User("user_five", new Password("five"), new HashSet<>((Arrays.asList((Role.PAGE_1), Role.PAGE_2, Role.PAGE_3)))));
+            saveUser(
+                    new User(
+                            "user_one",
+                            new Password("one"),
+                            new HashSet<>((Arrays.asList((Role.PAGE_1))))
+                    )
+            );
+            saveUser(
+                    new User(
+                            "user_two",
+                            new Password("two"),
+                            new HashSet<>((Arrays.asList((Role.PAGE_2))))
+                    )
+            );
+            saveUser(
+                    new User(
+                            "user_three",
+                            new Password("three"),
+                            new HashSet<>((Arrays.asList((Role.PAGE_3))))
+                    )
+            );
+            saveUser(
+                    new User("user_four",
+                            new Password("four"),
+                            new HashSet<>((Arrays.asList((Role.PAGE_1), Role.PAGE_2)))
+                    )
+            );
+            saveUser(
+                    new User("user_five",
+                            new Password("five"),
+                            new HashSet<>((Arrays.asList((Role.PAGE_1), Role.PAGE_2, Role.PAGE_3)))
+                    )
+            );
         } catch (Exception ignored) {
 
         }
@@ -29,7 +58,7 @@ public class UserRepositoryInMemory implements UserRepository{
 
     @Override
     public User findUserByUserName(String userName) throws UserNotExistsException {
-        User user  = users.get(userName);
+        User user = users.get(userName);
 
         if (user == null) {
             throw new UserNotExistsException();
@@ -37,6 +66,7 @@ public class UserRepositoryInMemory implements UserRepository{
 
         return user;
     }
+
     @Override
     public void saveUser(User user) throws UserAlreadyExistsException {
         if (this.users.get(user.getUserName()) != null) {
